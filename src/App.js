80ab73b7166d1c1bcb4f895ac2249324cd3255ef
Barbara Scelza <barbara.scelza@dualbootpartners.com>
2022-05-19 17:57:24 -0300

    Clase 1 - instalacion

--- src/App.js
+++ src/App.js
@@ -5,18 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>La Focacceria Barrio Sur</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bárbara Scelza 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
